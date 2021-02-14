14363051f47bf84481ed3b96c99f7dfb27b61fc9
tornado4242 <patrickcrowley4242@gmail.com>
2021-02-14 02:02:30 +0000

    eding some java files

---
+++ src/test/java/com/simplilearn/BookzyApp/BookzyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.BookzyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
