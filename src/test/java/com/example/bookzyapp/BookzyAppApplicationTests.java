623e488b2c45eb8267526c790c57de73caa3bb06
tornado4242 <patrickcrowley4242@gmail.com>
2021-02-18 16:50:39 -0500

    commiting files

--- src/test/java/com/example/bookzyapp/BookzyAppApplicationTests.java
+++ src/test/java/com/example/bookzyapp/BookzyAppApplicationTests.java
@@ -2,12 +2,64 @@ package com.example.bookzyapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-@SpringBootTest
-class BookzyAppApplicationTests {
+/**
+ * Created by 
+ * Sample junit test code to integrate by
+ */
+public class bookzyapp
+{
+    private static String Base_Url = "https://www.facebook.com";
+    private WebDriver driver;
 
-	@Test
-	void contextLoads() {
-	}
+    @Before
+    public void setUp()
+    {
+        driver = new ChromeDriver();
+        driver.get(Base_Url);
+    }
 
+    @After
+    public void after()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void testCasePassed()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
+    }
+
+    @Test
+    public void testCaseFailed()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+    }
+
+    @Ignore
+    @Test
+    public void testCaseIgnored()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
+    }
 }
+
+
+
+#@SpringBootTest
+#class BookzyAppApplicationTests {
+#
+#	@Test
+#	void contextLoads() {
+#	}
+#
+#}
