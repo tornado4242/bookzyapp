8904de5b514398f7b7ed3c9a1ae9da2c8d5e3421
tornado4242 <patrickcrowley4242@gmail.com>
2021-02-18 17:49:46 -0500

    adding files

--- src/test/java/com/example/bookzyapp/BookzyAppApplicationTests.java
+++ src/test/java/com/example/bookzyapp/BookzyAppApplicationTests.java
@@ -2,14 +2,14 @@ package com.example.bookzyapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.junit.After;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Ignore;
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
 @SpringBootTest
 class BookzyAppApplicationTests {
